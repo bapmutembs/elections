9146ce52ff49e0627948a4bf4d51aa80061e81a8
bapmutembs <bapmutembs@gmail.com>
2024-04-24 18:58:12 +0100

    add pages on main menu

--- lib/widgets/mainMenu.dart
+++ lib/widgets/mainMenu.dart
@@ -4,6 +4,10 @@ import 'package:elections/pages/Home/HomePage.dart';
 import 'package:elections/pages/Login/LoginPage.dart';
 import 'package:elections/pages/MissionList/MissionListPage.dart';
 import 'package:elections/pages/intro/introPage.dart';
+import 'package:elections/pages/offre/offreList.dart';
+import 'package:elections/pages/paiement/listpaiement.dart';
+import 'package:elections/pages/presence/listPresence.dart';
+import 'package:elections/pages/prime/pagePrime.dart';
 import 'package:elections/utils/Colors.dart';
 import 'package:elections/utils/Logo.dart';
 import 'package:flutter/material.dart';
@@ -56,6 +60,18 @@ class mainMenu {
               Navigator.push(
                 context,
                 MaterialPageRoute(
+                  builder: (ctx) => DepartementListPage(),
+                ),
+              );
+            },
+            title: Text("Departement"),
+            leading: Icon(Icons.business_outlined),
+          ),
+          ListTile(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
                   builder: (ctx) => MissionListPage(),
                 ),
               );
@@ -68,12 +84,48 @@ class mainMenu {
               Navigator.push(
                 context,
                 MaterialPageRoute(
-                  builder: (ctx) => DepartementListPage(),
+                  builder: (ctx) => OffrePage(),
                 ),
               );
             },
-            title: Text("Departement"),
-            leading: Icon(Icons.business_outlined),
+            title: Text("Offre Emploi"),
+            leading: Icon(Icons.local_offer),
+          ),
+          ListTile(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (ctx) => ListPaiement(),
+                ),
+              );
+            },
+            title: Text("Paiement"),
+            leading: Icon(Icons.monetization_on),
+          ),
+          ListTile(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (ctx) => ListPresence(),
+                ),
+              );
+            },
+            title: Text("Presence"),
+            leading: Icon(Icons.list_alt),
+          ),
+          ListTile(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (ctx) => PagePrime(),
+                ),
+              );
+            },
+            title: Text("Prime"),
+            leading: Icon(Icons.money_outlined),
           ),
           Divider(),
           ListTile(
